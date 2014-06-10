9746d14047149ded92f69a0dfea44e636f89b729
iluhcm <sam.lxing@gmail.com>
2014-06-10 11:22:00 +0800

    Add a method to judge whether the given name is a standard chromosome name.

--- red/com/xl/utils/ChromosomeUtils.java
+++ red/com/xl/utils/ChromosomeUtils.java
@@ -1,16 +1,32 @@
 package com.xl.utils;
 
 public class ChromosomeUtils {
-	public static String getStandardChromosomeName(String chr){
-		if(chr.length()==4){
-			return chr;
-		}
-		String chrName = chr.substring(0, 5);
-		Character c = chrName.charAt(4);
-		if(Character.isDigit(c)){
-			return chrName;
-		}else {
-			return null;
-		}
-	}
+    public static String getStandardChromosomeName(String chr) {
+        if (chr.length() == 4) {
+            return chr;
+        }
+        String chrName = chr.substring(0, 5);
+        Character c = chrName.charAt(4);
+        if (Character.isDigit(c)) {
+            return chrName;
+        } else {
+            return null;
+        }
+    }
+
+    public static boolean isStandardChromosomeName(String chr) {
+        int chrNameLength = chr.length();
+        if (chrNameLength < 6 && chr.substring(0, 3).equalsIgnoreCase("chr")) {
+            if (Character.isDigit(chr.charAt(3))) {
+                return true;
+            } else if (chr.toLowerCase().charAt(3) == 'x' || chr.toLowerCase().charAt(3) == 'y') {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
 }
