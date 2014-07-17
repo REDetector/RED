4649d6e3426edbaa31a03c4900e8f289c5df60e1
HerBigUncle <wubupt@foxmail.com>
2014-07-17 11:15:28 +0800

    Sola

---
+++ red/com/denovo/FileUtils.java
@@ -0,0 +1,23 @@
+package com.denovo;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileUtils {
+	public static File openFile(String path){
+		return new File(path);	
+	}
+	public final static Map<String,String> alter=new HashMap<String,String>();
+	
+	static {
+		for(int i=1;i<23;i++){
+			alter.put(i+"", "chr"+i);
+			alter.put("ch"+i, "chr"+i);
+			alter.put("Chr"+i, "chr"+i);
+		}
+		alter.put("M", "chrM");
+		alter.put("Y", "chrY");
+		alter.put("X", "chrX");
+	}
+}
