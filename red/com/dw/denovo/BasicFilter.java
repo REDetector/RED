b1da547b81a05ad36caa4ab0f71f8acbff64129f
HerBigUncle <wubupt@foxmail.com>
2014-07-19 21:45:56 +0800

    。

---
+++ red/com/dw/denovo/BasicFilter.java
@@ -0,0 +1,111 @@
+package com.dw.denovo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.dw.publicaffairs.DatabaseManager;
+import com.dw.publicaffairs.Utilities;
+
+/**
+ * Basic process for RNA-editing
+ * specific filter means we only focus on A-G
+ * basic filter means we set threshold on quality and depth
+ */
+public class BasicFilter {
+    private DatabaseManager databaseManager;
+    private String chr = null;
+    private String ps = null;
+    private String rnaVcf=null;
+    private String basicTable=null;
+    private String specificTable=null;
+
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private int ref_n = 0;
+    private int alt_n = 0;
+    private int count=0;
+
+    public BasicFilter(DatabaseManager databaseManager,String rnaVcf,String specificTable, String basicTable) {
+        this.databaseManager = databaseManager;
+        this.rnaVcf=rnaVcf;
+        this.basicTable = basicTable;
+        this.specificTable = specificTable;
+    }
+
+    public void createSpecificTable() {
+        databaseManager.deleteTable(specificTable);
+        databaseManager.createTable(specificTable, "(chrome varchar(30)," + Utilities.getInstance().getS2() + "," +
+                "index(chrome,pos))");
+    }
+
+    public void specificf() {
+        System.out.println("specific start" + " " + df.format(new Date()));
+        
+        databaseManager.insertClause("insert into " + specificTable + "(" + Utilities.getInstance().getS3() + ")  select * from " +
+                ""+rnaVcf+" where " + "REF='A' AND ALT='G'");
+        
+        System.out.println("specific end" + " " + df.format(new Date()));
+    }
+
+//    public void spePost() {
+//        System.out.println("post start" + " " + df.format(new Date()));
+//        
+//        databaseManager.executeSQL("create temporary table newtable select distinct * from " + specificTable);
+//        databaseManager.executeSQL("truncate table " + specificTable);
+//        databaseManager.executeSQL("insert into " + specificTable + " select * from  newtable");
+//        databaseManager.deleteTable("newTable");
+//        
+//        System.out.println("post end" + " " + df.format(new Date()));
+//    }
+
+    public void basicf(int qua, int num) {
+        try {
+            System.out.println("bfilter start" + " " + df.format(new Date()));// new Date()Ϊ��ȡ��ǰϵͳʱ��
+
+            //create table and insert data into it
+            databaseManager.deleteTable(basicTable);
+            databaseManager.createTable(basicTable, "(chrome varchar(30)," + Utilities.getInstance().getS2() + ",index(chrome,pos))");
+            ResultSet rs = databaseManager.query(specificTable, "chrome,pos,AD","1");
+            List<String> coordinate = new ArrayList<String>();
+            databaseManager.setAutoCommit(false);
+            
+            while (rs.next()) {
+                coordinate.add(rs.getString(1));
+                coordinate.add(rs.getString(2));
+                coordinate.add(rs.getString(3));
+            }
+            for (int i = 0, len = coordinate.size(); i < len; i++) {
+                switch (i % 3) {
+                    case 0:
+                        chr = coordinate.get(i);
+                        break;
+                    case 1:
+                        ps = coordinate.get(i);
+                        break;
+                    case 2:
+                        String[] section = coordinate.get(i).split(";");
+                        ref_n = Integer.parseInt(section[0]);
+                        alt_n = Integer.parseInt(section[1]);
+                        if (ref_n + alt_n > num) {
+                            databaseManager.executeSQL("insert into " + basicTable + "( " + Utilities.getInstance().getS3() +
+                                    ") (select * from " + specificTable + " where filter='PASS' and pos=" + ps
+                                    + " and qual >" + qua + " and chrome='" + chr + "')");
+                            count++;
+                            if(count%10000==0)
+                            	databaseManager.commit();
+                        }
+                        break;
+                }
+                databaseManager.commit();
+            }
+            databaseManager.setAutoCommit(true);
+            System.out.println("bfilter end" + " " + df.format(new Date()));
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ red/com/dw/denovo/RepeatFilter.java
@@ -0,0 +1,148 @@
+package com.dw.denovo;
+
+/**
+ * we will filter out base in repeated area except for SINE/alu
+ */
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.dw.publicaffairs.DatabaseManager;
+import com.dw.publicaffairs.Utilities;
+
+public class RepeatFilter {
+	private DatabaseManager databaseManager;
+
+	private String repeatIn=null;
+	private String repeatTable=null;
+	private String referenceRepeat=null;
+	private String refTable=null;
+	FileInputStream inputStream;
+	private String line=null;
+	private int count=3;
+	private int index=0;
+	private String chr =null;
+	private String ps  =null;
+
+	// �������ڸ�ʽ
+	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	public RepeatFilter(DatabaseManager databaseManager,String repeatIn,String repeatTable,String referenceRepeat,String refTable) {
+        this.databaseManager = databaseManager;
+        this.repeatIn = repeatIn;
+        this.repeatTable=repeatTable;
+        this.referenceRepeat=referenceRepeat;
+        this.refTable=refTable;
+    }
+	
+	public void loadrepeat(){
+		try {
+			System.out.println("loadrepeat start"+" "+df.format(new Date()));// new Date()Ϊ��ȡ��ǰϵͳʱ��
+			int ts_count = 0;
+			try {
+				inputStream = new FileInputStream(repeatIn);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			databaseManager.deleteTable(referenceRepeat);
+			//column index
+			databaseManager.createTable(referenceRepeat, "(chrome varchar(25),begin int,end int,type varchar(40),index(chrome))");
+			databaseManager.setAutoCommit(false);
+			BufferedReader rin = new BufferedReader(new InputStreamReader(inputStream));		
+            while((line=rin.readLine())!=null){
+            	//clear head of fa.out
+            	line=line.replaceAll("['   ']+","\t");
+            	if(count>0)
+            		{
+            		count--;
+            		continue;
+            		}
+            	if(!line.startsWith("\t"))
+            		index--;
+            	if(line.split("\t")[index+5].length()>6)
+            	{
+            		index=0;
+            		continue;
+            	}            		
+            	databaseManager.executeSQL("insert into "+referenceRepeat+"(chrome,begin,end,type) values('"+line.split("\t")[index+5]+"','"+line.split("\t")[index+6]+"','"+line.split("\t")[index+7]+"','"+line.split("\t")[index+11]+"')");
+            	ts_count++;
+            	if(ts_count%30000==0)
+    			databaseManager.commit();
+    			index=0;
+            }
+            databaseManager.commit();
+            databaseManager.setAutoCommit(true);
+            System.out.println("loadrepeat end"+" "+df.format(new Date()));// new Date()Ϊ��ȡ��ǰϵͳʱ��	
+		} catch ( IOException e) {
+			// TODO Auto-generated catch block 
+			e.printStackTrace();
+		} 
+	}
+	
+	public void establishrepeat(){
+			System.out.println("esrepeat start"+" "+df.format(new Date()));// new Date()Ϊ��ȡ��ǰϵͳʱ��
+			
+			databaseManager.deleteTable(repeatTable);
+			databaseManager.createTable(repeatTable, "(chrome text,"+Utilities.getInstance().getS2()+")");
+
+			System.out.println("esrepeat end"+" "+df.format(new Date()));
+	}
+	
+	public void rfilter(){
+			try {
+				System.out.println("rfliter start"+" "+df.format(new Date()));// new Date()Ϊ��ȡ��ǰϵͳʱ��
+				ResultSet rs=databaseManager.query(refTable, "chrome,pos", "1");
+				List<String> coordinate = new ArrayList<String>();
+	            databaseManager.setAutoCommit(false);
+	            
+				 while (rs.next()) {
+		                coordinate.add(rs.getString(1));
+		                coordinate.add(rs.getString(2));
+		            }
+		            for (int i = 0, len = coordinate.size(); i < len; i++) {
+		                switch (i % 2) {
+		                    case 0:
+		                        chr = coordinate.get(i);
+		                        break;
+		                    case 1:
+		                        ps = coordinate.get(i);
+		                        rs=databaseManager.query(referenceRepeat, "type", "(chrome='"+chr+"' and begin<"+ps+" and end>"+ps+")");
+		                        if(!rs.next())
+		                		{
+		                			databaseManager.executeSQL("insert into "+repeatTable+"  select * from "+refTable+" where chrome='"+chr+"' and pos="+ps+"");
+		                			count++;
+		                			if(count%10000==0)
+		                				databaseManager.commit();
+		                		}
+		                		//SINEalu is also what we need
+		                		else if(rs.next()&&rs.getString(1)=="SINE/Alu")
+		                		{
+		                			databaseManager.executeSQL("insert into "+repeatTable+"  select * from "+refTable+" where chrome='"+chr+"' and pos="+ps+"");
+		                			count++;
+		                			if(count%10000==0)
+		                				databaseManager.commit();
+		                		}
+		                        break;
+		                }
+		            }
+	
+		            databaseManager.commit();
+		            databaseManager.setAutoCommit(true);
+
+		System.out.println("rfilter end"+" "+df.format(new Date()));// new Date()Ϊ��ȡ��ǰϵͳʱ��
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
