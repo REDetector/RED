930910e4f149a3d3ca8405a6fa6a6229cfff9419
iluhcm <sam.lxing@gmail.com>
2014-11-02 16:10:14 +0800

    Rename QCFilter to QualityControlFilter.

--- red/com/dw/denovo/QualityControlFilter.java
+++ red/com/dw/denovo/QualityControlFilter.java
@@ -1,7 +1,7 @@
 package com.dw.denovo;
 
-import com.dw.publicaffairs.DatabaseManager;
-import com.xl.datatypes.probes.ProbeBean;
+import com.dw.dbutils.DatabaseManager;
+import com.xl.datatypes.sites.SiteBean;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,12 +13,12 @@ import java.util.List;
 /**
  * Basic process for QC filter means we set threshold on quality and depth
  */
-public class QCFilter {
+public class QualityControlFilter {
     private DatabaseManager databaseManager;
     private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private int count = 0;
 
-    public QCFilter(DatabaseManager databaseManager) {
+    public QualityControlFilter(DatabaseManager databaseManager) {
         this.databaseManager = databaseManager;
     }
 
@@ -31,20 +31,20 @@ public class QCFilter {
         try {
             System.out.println("Start executing QC filter..." + df.format(new Date()));
             ResultSet rs = databaseManager.query(refTable, "chrom,pos,AD", "1");
-            List<ProbeBean> probeBeans = new ArrayList<ProbeBean>();
+            List<SiteBean> siteBeans = new ArrayList<SiteBean>();
             while (rs.next()) {
-                ProbeBean probeBean = new ProbeBean(rs.getString(1), rs.getInt(2));
-                probeBean.setAd(rs.getString(3));
-                probeBeans.add(probeBean);
+                SiteBean siteBean = new SiteBean(rs.getString(1), rs.getInt(2));
+                siteBean.setAd(rs.getString(3));
+                siteBeans.add(siteBean);
             }
             databaseManager.setAutoCommit(false);
-            for (ProbeBean probeBean : probeBeans) {
-                String[] sections = probeBean.getAd().split("/");
+            for (SiteBean siteBean : siteBeans) {
+                String[] sections = siteBean.getAd().split("/");
                 int ref_n = Integer.parseInt(sections[0]);
                 int alt_n = Integer.parseInt(sections[1]);
                 if (ref_n + alt_n >= depth) {
-                    databaseManager.executeSQL("insert into " + basicTable + " (select * from " + refTable + " where filter='PASS' and pos=" + probeBean
-                            .getPos() + " and qual >=" + quality + " and chrom='" + probeBean.getChr() + "')");
+                    databaseManager.executeSQL("insert into " + basicTable + " (select * from " + refTable + " where filter='PASS' and pos=" + siteBean
+                            .getPos() + " and qual >=" + quality + " and chrom='" + siteBean.getChr() + "')");
                     if (++count % DatabaseManager.COMMIT_COUNTS_PER_ONCE == 0)
                         databaseManager.commit();
                 }
