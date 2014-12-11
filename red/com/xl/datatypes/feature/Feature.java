1a066cb23cb94b71e7666b8e902016f1a0d31938
iluhcm <sam.lxing@gmail.com>
2014-12-11 11:05:53 +0800

    A Feature represents a strip of information obtained from UCSC gene annotation file(i.e., gene.gtf).

--- red/com/xl/datatypes/feature/Feature.java
+++ red/com/xl/datatypes/feature/Feature.java
@@ -1,3 +1,21 @@
+/*
+ * RED: RNA Editing Detector
+ *     Copyright (C) <2014>  <Xing Li>
+ *
+ *     RED is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     RED is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.xl.datatypes.feature;
 
 import com.xl.datatypes.sequence.Location;
@@ -6,14 +24,30 @@ import com.xl.utils.Strand;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * A Feature represents a strip of information obtained from UCSC gene annotation file(i.e., gene.gtf).
+ */
 public class Feature implements Comparable<Feature>, Serializable {
 
+    /**
+     * The feature name.
+     */
     private String name = null;
+    /**
+     * The chromosome name.
+     */
     private String chr = null;
+    /**
+     * The strand of this feature.
+     */
     private Strand strand = null;
-
-
+    /**
+     * The location, including transcribed region, coding region and exon region.
+     */
     private List<Location> allLocations;
+    /**
+     * The alias name(e.g. gene_id from GTF)
+     */
     private String aliasName = null;
 
     public Feature(String name, String chr, Strand strand, List<Location> allLocations, String aliasName) {
@@ -57,19 +91,8 @@ public class Feature implements Comparable<Feature>, Serializable {
         return aliasName;
     }
 
-    /**
-     * Get human readable locus string. It is assumed that this.start and
-     * this.end are 0-based and end-exclusive, and the returned string is
-     * 1-based and end-inclusive. So basically we just add 1 to the start.
-     *
-     * @return
-     */
-//    public String getLocusString() {
-//        return getChr() + ":" + (txLocation.getStart() + 1) + "-" + txLocation.getEnd();
-//    }
     @Override
     public int compareTo(Feature o) {
-        // TODO Auto-generated method stub
         Location txLocation = allLocations.get(0);
         if (txLocation.getStart() != o.getTxLocation().getStart()) {
             return (txLocation.getStart() - o.getTxLocation().getStart());
@@ -80,24 +103,8 @@ public class Feature implements Comparable<Feature>, Serializable {
         }
     }
 
-//    @Override
-//    public String toString() {
-//        return name + "\t" + chr + "\t" + Strand.parseStrand(strand) + "\t" + txLocation.toString() + "\t" + cdsLocation.toString() + "\t"
-//                + toExonString() + "\t" + aliasName;
-//    }
-
     public int getTotalLength() {
         return getTxLocation().getEnd() - getTxLocation().getStart();
     }
-
-//    public String toExonString() {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        for (Location exon : exonLocations) {
-//            stringBuilder.append(exon.toString());
-//            stringBuilder.append(",");
-//        }
-//        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-//        return stringBuilder.toString();
-//    }
 
 }
