156c86f8cbdc2848ac49727245e7aef7102c3df2
iluhcm <sam.lxing@gmail.com>
2014-12-11 11:29:51 +0800

    The Class DataTreeRenderer sets the look of the DataViewer.

--- red/com/xl/display/dataviewer/DataTreeRenderer.java
+++ red/com/xl/display/dataviewer/DataTreeRenderer.java
@@ -1,24 +1,22 @@
-package com.xl.display.dataviewer;
-
-/**
- * Copyright Copyright 2007-13 Simon Andrews
+/*
+ * RED: RNA Editing Detector
+ *     Copyright (C) <2014>  <Xing Li>
  *
- *    This file is part of SeqMonk.
+ *     RED is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *    SeqMonk is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 3 of the License, or
- *    (at your option) any later version.
+ *     RED is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *    SeqMonk is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with SeqMonk; if not, write to the Free Software
- *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package com.xl.display.dataviewer;
 
 import com.xl.datatypes.DataGroup;
 import com.xl.datatypes.DataSet;
@@ -30,53 +28,41 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
 
 /**
- * The Class DataTreeRenderer sets the look of the DataTree
+ * The Class DataTreeRenderer sets the look of the DataViewer.
  */
 public class DataTreeRenderer extends DefaultTreeCellRenderer {
 
     /**
      * The data set icon.
      */
-    private static Icon dataSetIcon = new ImageIcon(
-            ClassLoader
-                    .getSystemResource("resources/dataset_icon.png"));
+    private Icon dataSetIcon = new ImageIcon(ClassLoader.getSystemResource("resources/dataset_icon.png"));
 
     /**
      * The data group icon.
      */
-    private static Icon dataGroupIcon = new ImageIcon(
-            ClassLoader
-                    .getSystemResource("resources/datagroup_icon.png"));
+    private Icon dataGroupIcon = new ImageIcon(ClassLoader.getSystemResource("resources/datagroup_icon.png"));
 
     /**
      * The site list icon.
      */
-    private static Icon siteListIcon = new ImageIcon(
-            ClassLoader
-                    .getSystemResource("resources/sitelist_icon.png"));
+    private Icon siteListIcon = new ImageIcon(ClassLoader.getSystemResource("resources/sitelist_icon.png"));
 
     /**
      * The annotation set icon.
      */
-    private static Icon annotationSetIcon = new ImageIcon(
-            ClassLoader
-                    .getSystemResource("resources/annotation_set_icon.png"));
+    private Icon annotationSetIcon = new ImageIcon(ClassLoader.getSystemResource("resources/annotation_set_icon.png"));
 
     /*
      * (non-Javadoc)
      *
      * @see
-     * javax.swing.tree.DefaultTreeCellRenderer#getTreeCellRendererComponent
-     * (javax.swing.JTree, java.lang.Object, boolean, boolean, boolean, int,
+     * javax.swing.tree.DefaultTreeCellRenderer#getTreeCellRendererComponent (javax.swing.JTree, java.lang.Object, boolean, boolean, boolean, int,
      * boolean)
      */
-    public Component getTreeCellRendererComponent(JTree tree, Object value,
-                                                  boolean selected, boolean expanded, boolean leaf, int row,
-                                                  boolean hasFocus) {
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
 
         if (value instanceof DataSet) {
-            JLabel label = new JLabel(value.toString(), dataSetIcon,
-                    JLabel.LEFT);
+            JLabel label = new JLabel(value.toString(), dataSetIcon, JLabel.LEFT);
 
             if (selected) {
                 label.setOpaque(true);
@@ -84,8 +70,7 @@ public class DataTreeRenderer extends DefaultTreeCellRenderer {
             }
             return label;
         } else if (value instanceof DataGroup) {
-            JLabel label = new JLabel(value.toString(), dataGroupIcon,
-                    JLabel.LEFT);
+            JLabel label = new JLabel(value.toString(), dataGroupIcon, JLabel.LEFT);
 
             if (selected) {
                 label.setOpaque(true);
@@ -93,24 +78,21 @@ public class DataTreeRenderer extends DefaultTreeCellRenderer {
             }
             return label;
         } else if (value instanceof SiteList) {
-            JLabel label = new JLabel(value.toString(), siteListIcon,
-                    JLabel.LEFT);
+            JLabel label = new JLabel(value.toString(), siteListIcon, JLabel.LEFT);
             if (selected) {
                 label.setOpaque(true);
                 label.setBackground(Color.LIGHT_GRAY);
             }
             return label;
         } else if (value instanceof AnnotationSet) {
-            JLabel label = new JLabel(value.toString(), annotationSetIcon,
-                    JLabel.LEFT);
+            JLabel label = new JLabel(value.toString(), annotationSetIcon, JLabel.LEFT);
             if (selected) {
                 label.setOpaque(true);
                 label.setBackground(Color.LIGHT_GRAY);
             }
             return label;
         } else {
-            return super.getTreeCellRendererComponent(tree, value, selected,
-                    expanded, leaf, row, hasFocus);
+            return super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
         }
     }
 
