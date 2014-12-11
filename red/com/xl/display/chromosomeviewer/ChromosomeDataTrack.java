8650eca2541657da4b3736de06abfa14cfdb5cff
iluhcm <sam.lxing@gmail.com>
2014-12-11 11:26:13 +0800

    The Class ChromosomeDataTrack represents a single track in the chromosome view containing the data from a single data store. Depending on the display preferences it can show either just mapped reads data, or RNA editing sites data, or both.

--- red/com/xl/display/chromosomeviewer/ChromosomeDataTrack.java
+++ red/com/xl/display/chromosomeviewer/ChromosomeDataTrack.java
@@ -1,16 +1,30 @@
+/*
+ * RED: RNA Editing Detector
+ *     Copyright (C) <2014>  <Xing Li>
+ *
+ *     RED is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     RED is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.xl.display.chromosomeviewer;
 
-import com.xl.datatypes.DataCollection;
-import com.xl.datatypes.DataGroup;
-import com.xl.datatypes.DataSet;
 import com.xl.datatypes.DataStore;
 import com.xl.datatypes.genome.Chromosome;
 import com.xl.datatypes.sequence.Location;
 import com.xl.datatypes.sequence.SequenceRead;
 import com.xl.datatypes.sites.Site;
 import com.xl.datatypes.sites.SiteList;
-import com.xl.datatypes.sites.SiteSet;
-import com.xl.interfaces.DataChangeListener;
+import com.xl.interfaces.ActiveDataChangedListener;
 import com.xl.parsers.dataparsers.BAMFileParser;
 import com.xl.preferences.DisplayPreferences;
 import com.xl.utils.AsciiUtils;
@@ -24,12 +38,17 @@ import java.util.*;
 import java.util.List;
 
 /**
- * The Class ChromosomeDataTrack represents a single track in the chromosome
- * view containing the data from a single data store. Depending on the display
- * preferences it can show either just raw data, or quantitated data, or both.
+ * The Class ChromosomeDataTrack represents a single track in the chromosome view containing the data from a single data store. Depending on the display
+ * preferences it can show either just mapped reads data, or RNA editing sites data, or both.
  */
-public class ChromosomeDataTrack extends AbstractTrack implements DataChangeListener {
+public class ChromosomeDataTrack extends AbstractTrack implements ActiveDataChangedListener {
+    /**
+     * If the viewer length is longer than ten million, then we show nothing but a notification to tell user zoom in.
+     */
     private static int TEN_MB = 10000000;
+    /**
+     * The chromosome.
+     */
     private Chromosome chromosome = null;
     /**
      * The data.
@@ -39,17 +58,10 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
      * The reads.
      */
     private List<? extends Location> reads = null;
-
-    private List<Site> depths = null;
-
     /**
      * The sites
      */
     private Site[] sites;
-    /**
-     * Whether the tip is displayed.
-     */
-    private boolean timing = true;
     /**
      * The drawn reads.
      */
@@ -70,33 +82,60 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
      * Each read's height index.
      */
     private int[] readsYIndex = null;
-
-    private int maxCoverage = 0;
-
+    /**
+     * Coverages of depth for the RNA editing sites which had been shown on the display screen.
+     */
+    private List<Site> depths = null;
+    /**
+     * The max of the coverage of depth.
+     */
+    private int maxDepth = 0;
+    /**
+     * Whether the tip is displayed.
+     */
+    private boolean timing = true;
+    /**
+     * Show sites or not, depending on the display preference.
+     */
     private boolean drawSites = true;
+    /**
+     * Show reads or not, depending on the display preference.
+     */
     private boolean drawReads = true;
-
-    private boolean isStandardChromosomeName;
-
+    /**
+     * A mechanism to reduce loading data frequently from the BAM file.
+     * <p/>
+     * Each time loading data, enlargeStart = currentViewerStart - viewerLength / 2
+     * <p/>
+     * If the currentViewerStart < enlargeStart, then load new data from BAM file again.
+     */
     private int enlargeStart = 0;
+    /**
+     * A mechanism to reduce loading data frequently from the BAM file.
+     * <p/>
+     * Each time loading data, enlargeEnd = currentViewerEnd + viewerLength / 2
+     * <p/>
+     * If the enlargeEnd < currentViewerEnd, then load new data from BAM file again.
+     */
     private int enlargeEnd = 0;
 
     /**
      * Instantiates a new chromosome data track.
      *
-     * @param viewer the viewer
-     * @param data   the data
+     * @param viewer The chromosome viewer which holds this track.
+     * @param data   The data which represents this data track.
      */
-    public ChromosomeDataTrack(ChromosomeViewer viewer, DataCollection collection, DataStore data) {
-        super(viewer, collection, data.name());
+    public ChromosomeDataTrack(ChromosomeViewer viewer, DataStore data) {
+        super(viewer, data.name());
         this.data = data;
-        isStandardChromosomeName = data.isStandardChromosomeName();
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         int viewerLength = getViewerLength();
+
+        // If viewer length is longer than ten million, then show a prompt to zoom in.
         if (viewerLength > TEN_MB) {
             g.setColor(ColourScheme.DATA_TRACK);
             String prompt = "Zoom in to make the mapped reads visible.";
@@ -107,6 +146,7 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
             enlargeStart = enlargeEnd = 0;
             return;
         }
+        // Display mode, show reads, show sites, show reads and sites.
         switch (DisplayPreferences.getInstance().getDisplayMode()) {
             case DisplayPreferences.DISPLAY_MODE_PROBES_ONLY:
                 drawReads = false;
@@ -120,6 +160,7 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
         }
         drawnReads.removeAllElements();
 
+        // Update enlarge start and end if needed.
         if (currentViewerStart < enlargeStart || currentViewerEnd > enlargeEnd) {
             if (viewerLength > displayWidth * 2) {
                 updateBlocks();
@@ -133,7 +174,9 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
                 enlargeEnd = chromosome.getLength();
             }
         }
+
         if (drawReads) {
+            // If viewer length is longer than twice display width, we display the reads in block status
             if (viewerLength > displayWidth * 2) {
                 drawBlocks(g);
                 reads = null;
@@ -164,6 +207,11 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
 
     }
 
+    /**
+     * Draw the reads from a given location.
+     *
+     * @param g the g
+     */
     private void drawReads(Graphics g) {
         if (reads == null) {
             updateReads();
@@ -182,13 +230,22 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
         }
     }
 
-    private void drawRead(Graphics g, Location r, int pixelYStart) {
-        g.setFont(FontManager.defaultFont);
-        int start = r.getStart();
-        int end = r.getEnd();
-        drawnReads.add(new DrawnRead(start, end, pixelYStart, pixelYStart + readHeight, r));
-        if (r instanceof SequenceRead) {
-            SequenceRead sequenceRead = (SequenceRead) r;
+    /**
+     * Draw a single read obtained from a BAM file.
+     *
+     * @param g           the g
+     * @param read        the read
+     * @param pixelYStart the y position refer to the screen pixel
+     */
+    private void drawRead(Graphics g, Location read, int pixelYStart) {
+        g.setFont(FontManager.DEFAULT_FONT);
+        int start = read.getStart();
+        int end = read.getEnd();
+        // Store this read to make it convenient find the active read.
+        drawnReads.add(new DrawnRead(start, end, pixelYStart, pixelYStart + readHeight, read));
+        if (read instanceof SequenceRead) {
+            SequenceRead sequenceRead = (SequenceRead) read;
+            // Get the small piece sequences which depends on the CIGAR string of this read.
             List<SequenceRead.SmallPieceSequence> smallPieceSequences = sequenceRead.getAlignmentBlocks();
             for (int i = 0, len = smallPieceSequences.size(); i < len; i++) {
                 SequenceRead.SmallPieceSequence smallPieceSequence = smallPieceSequences.get(i);
@@ -207,6 +264,7 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
                         drawBase(g, c, start + k, pixelYStart + readHeight);
                     }
                 }
+                // A line for a splice junction between two small piece sequences.
                 if (i < len - 1) {
                     g.setColor(ColourScheme.READ_INTEVAL);
                     drawLine(g, start + k, smallPieceSequences.get(i + 1).getReferenceStart(), pixelYStart + readHeight / 2, pixelYStart + readHeight / 2);
@@ -219,6 +277,11 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
         }
     }
 
+    /**
+     * Draw the RNA editing sites.
+     *
+     * @param g the g
+     */
     private void drawSites(Graphics g) {
         if (sites == null) {
             return;
@@ -230,16 +293,28 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
         }
     }
 
-    private void drawSite(Graphics g, Site b) {
-        g.setColor(ColourScheme.getBaseColor(b.getAltBase()));
-        int position = b.getStart();
-        int baseWidth = g.getFontMetrics().stringWidth(String.valueOf(b.getAltBase()));
+    /**
+     * Draw a site in a box using two line, whose color is the same as alternative base color.
+     *
+     * @param g    the g
+     * @param site the site
+     */
+    private void drawSite(Graphics g, Site site) {
+        g.setColor(ColourScheme.getBaseColor(site.getAltBase()));
+        int position = site.getStart();
+        int baseWidth = g.getFontMetrics().stringWidth(String.valueOf(site.getAltBase()));
         int left = bpToPixel(position) - basePixel / 2 + baseWidth / 2;
         int right = bpToPixel(position) + basePixel / 2 + baseWidth / 2;
         g.drawLine(left, 0, left, displayHeight);
         g.drawLine(right, 0, right, displayHeight);
     }
 
+    /**
+     * If the viewer is shorter than ten million and longer than twice screen pixel, we have a comfortable view with the block status to tell user about the
+     * distribution of mapped reads.
+     *
+     * @param g the g
+     */
     private void drawBlocks(Graphics g) {
         if (depths == null) {
             return;
@@ -250,32 +325,45 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
             if (position >= currentViewerStart && position <= currentViewerEnd) {
                 drawLine(g, position, position, 0, depth.getDepth() * readHeight);
             }
-
         }
     }
 
+    /**
+     * Update the read list by loading data from BAM file when conditions (currentViewerStart < enlargeStart || currentViewerEnd > enlargeEnd) can not be
+     * matched. Before query for new data, we need to know whether the chromosome name is standard (i.e., chr1, ch1, 1).
+     */
     private void updateReads() {
         if (chromosome == null) {
             return;
         }
         String currentChromosome = chromosome.getName();
-        if (!isStandardChromosomeName) {
+        if (!data.isStandardChromosomeName()) {
             currentChromosome = ChromosomeUtils.getAliasChromosomeName(currentChromosome);
+        }
+        if (reads != null) {
+            reads.clear();
+            reads = null;
+            System.gc();
         }
         reads = data.getDataParser().query(currentChromosome, enlargeStart, enlargeEnd);
         readsYIndex = new int[reads.size()];
         Arrays.fill(readsYIndex, -1);
-        maxCoverage = processSequence(reads, readsYIndex);
-        setPreferredSize(new Dimension(displayWidth, maxCoverage * readHeight));
+        // Every time data has been loaded to read list, we need to calculate the display position for each read, especially the height index.
+        maxDepth = processSequence(reads, readsYIndex);
+        // When the max coverage of depth has been calculated, we have to set the preferred size in order to adjusting the scroll bar to its best position.
+        setPreferredSize(new Dimension(displayWidth, maxDepth * readHeight));
         repaint();
     }
 
+    /**
+     * Update the block status
+     */
     private void updateBlocks() {
         if (chromosome == null) {
             return;
         }
         String currentChromosome = chromosome.getName();
-        if (!isStandardChromosomeName) {
+        if (!data.isStandardChromosomeName()) {
             currentChromosome = ChromosomeUtils.getAliasChromosomeName(currentChromosome);
         }
         if (depths != null) {
@@ -285,11 +373,11 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
         }
         depths = ((BAMFileParser) data.getDataParser()).getDepth(currentChromosome, currentViewerStart, currentViewerEnd);
         for (Site site : depths) {
-            if (maxCoverage < site.getDepth()) {
-                maxCoverage = site.getDepth();
+            if (maxDepth < site.getDepth()) {
+                maxDepth = site.getDepth();
             }
         }
-        setPreferredSize(new Dimension(displayWidth, maxCoverage * readHeight));
+        setPreferredSize(new Dimension(displayWidth, maxDepth * readHeight));
         repaint();
     }
 
@@ -300,17 +388,15 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
     protected void updateTrack(Chromosome chromosome) {
         this.chromosome = chromosome;
         String currentChromosome = chromosome.getName();
-        if (dataCollection.siteSet() != null) {
-            sites = dataCollection.siteSet().getActiveList().getSitesForChromosome(currentChromosome);
+        if (data.siteSet() != null) {
+            sites = data.siteSet().getActiveList().getSitesForChromosome(currentChromosome);
         } else {
             sites = new Site[0];
         }
         Arrays.sort(sites);
         if (getViewerLength() > TEN_MB) {
             repaint();
-            return;
-        }
-        if (getViewerLength() > displayWidth * 2) {
+        } else if (getViewerLength() > displayWidth * 2) {
             updateBlocks();
         } else {
             updateReads();
@@ -318,7 +404,7 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
     }
 
     /**
-     * Find read.
+     * Find read from the drawn reads vector.
      *
      * @param x the x
      * @param y the y
@@ -339,90 +425,64 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
         }
     }
 
+    /**
+     * An efficient way to calculate the y index for each read so we can show the reads without any overlap in the chromosome view. We judge whether the new
+     * read's start position is overlapped with all of the indexes of last read's end positions (stored in a list named lastYIndexes), if it is overlapped with
+     * all, then add this read's end position to the index list.
+     *
+     * @param reads       The reads
+     * @param readsYIndex The calculated y index for each read.
+     * @return the max coverage of depth calculated by ourselves.
+     */
     private int processSequence(List<? extends Location> reads, int[] readsYIndex) {
         if (reads == null || reads.size() == 0) {
             return 0;
         }
+        // A list to hold all last y indexes.
         List<Integer> lastYIndexes = new ArrayList<Integer>();
         lastYIndexes.add(reads.get(0).getEnd());
         int readsLength = reads.size();
         readsYIndex[0] = 0;
         for (int i = 1; i < readsLength; i++) {
             int j = 0;
+            Location currentRead = reads.get(i);
             int yIndexLength = lastYIndexes.size();
-            int currentReadStart = reads.get(i).getStart();
+            int currentReadStart = currentRead.getStart();
             for (; j < yIndexLength; j++) {
+                // Judge if it is overlapped, if not, then update the last y index of this height to the new read's end position.
                 if (currentReadStart - lastYIndexes.get(j) > 0) {
+                    // Set y index for this read.
                     readsYIndex[i] = j;
-                    lastYIndexes.set(j, reads.get(i).getEnd());
+                    // Update the last y index for the list.
+                    lastYIndexes.set(j, currentRead.getEnd());
+                    // Stop the for circulation.
                     j = yIndexLength;
                 }
             }
+            // If the new read is overlapped with all of the last y indexes, then add the new last y index to the list.
             if (readsYIndex[i] == -1) {
                 readsYIndex[i] = j + 1;
-                lastYIndexes.add(reads.get(i).getEnd());
+                lastYIndexes.add(currentRead.getEnd());
             }
         }
         return lastYIndexes.size();
     }
 
+    /**
+     * We have to override this method to make the scroll pane visible.
+     *
+     * @return the preferred size.
+     */
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(0, maxCoverage * readHeight);
+        return new Dimension(0, maxDepth * readHeight);
     }
 
     @Override
-    public void dataSetAdded(DataSet d) {
-
-    }
-
-    @Override
-    public void dataSetsRemoved(DataSet[] d) {
-
-    }
-
-    @Override
-    public void dataGroupAdded(DataGroup g) {
-
-    }
-
-    @Override
-    public void dataGroupsRemoved(DataGroup[] g) {
-
-    }
-
-    @Override
-    public void dataSetRenamed(DataSet d) {
-
-    }
-
-    @Override
-    public void dataGroupRenamed(DataGroup g) {
-
-    }
-
-    @Override
-    public void dataGroupSamplesChanged(DataGroup g) {
-
-    }
-
-    @Override
-    public void siteSetReplaced(SiteSet p) {
-        if (p == null) {
-            sites = null;
-        } else {
-            sites = p.getSitesForChromosome(DisplayPreferences.getInstance().getCurrentChromosome().getName());
-            Arrays.sort(sites);
+    public void activeDataChanged(DataStore d, SiteList l) {
+        if (data != d) {
+            return;
         }
-    }
-
-    @Override
-    public void activeDataStoreChanged(DataStore s) {
-
-    }
-
-    @Override
-    public void activeSiteListChanged(SiteList l) {
         if (l == null) {
             sites = null;
         } else {
@@ -442,9 +502,8 @@ public class ChromosomeDataTrack extends AbstractTrack implements DataChangeList
     @Override
     public void mouseMoved(MouseEvent me) {
         /*
-         * In many cases we don't need to search through reads and sites, so we
-		 * can quickly work out what we should be looking for from what we're
-		 * drawing and where the mouse is.
+         * In many cases we don't need to search through reads and sites, so we can quickly work out what we should be looking for from what we're drawing
+         * and where the mouse is.
 		 */
         findRead(pixelToBp(me.getX()), me.getY());
         long lastTime = System.currentTimeMillis();
