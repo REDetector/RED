9690db2314eca4607e3aee38ef769ea688f952f1
iluhcm <sam.lxing@gmail.com>
2014-12-11 11:18:06 +0800

    The Class DataGroup is a virtual DataStore which can combine one or more DataSets. It just contains the references of the combined DataSets and don't occupy extra memory or local disk.

--- red/com/xl/datatypes/DataGroup.java
+++ red/com/xl/datatypes/DataGroup.java
@@ -1,21 +1,19 @@
-/**
- * Copyright Copyright 2007-13 Simon Andrews
+/*
+ * RED: RNA Editing Detector
+ *     Copyright (C) <2014>  <Xing Li>
  *
- *    This file is part of SeqMonk.
+ *     RED is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *    SeqMonk is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 3 of the License, or
- *    (at your option) any later version.
+ *     RED is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *    SeqMonk is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with SeqMonk; if not, write to the Free Software
- *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.xl.datatypes;
 
@@ -28,17 +26,15 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * The Class DataGroup is a virtual DataStore which can combine
- * one or more DataSets.  It does not store read information, but
- * does store its own quantitated data.
+ * The Class DataGroup is a virtual DataStore which can combine one or more DataSets. It just contains the references of the combined DataSets and don't occupy
+ * extra memory or local disk.
  */
 public class DataGroup extends DataStore {
 
     /**
-     * The data sets.
+     * The data sets which this data group contains.
      */
     private DataSet[] dataSets;
-
 
     /**
      * Instantiates a new data group.
@@ -60,7 +56,6 @@ public class DataGroup extends DataStore {
         return dataSets;
     }
 
-
     /**
      * Sets the data sets.
      *
@@ -76,7 +71,7 @@ public class DataGroup extends DataStore {
     public void setName(String name) {
         super.setName(name);
         if (collection() != null) {
-            collection().dataGroupRenamed(this);
+            collection().dataStoreRenamed(this);
         }
     }
 
