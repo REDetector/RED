053c4c80eed5e727a221a494ffcbe49cd5821d77
iluhcm <sam.lxing@gmail.com>
2014-12-13 17:02:31 +0800

    The Class ChromosomeDisplay shows a single chromosome within the genome view.

--- red/com/xl/display/genomeviewer/ChromosomeScale.java
+++ red/com/xl/display/genomeviewer/ChromosomeScale.java
@@ -1,3 +1,21 @@
+/*
+ * RED: RNA Editing Detector
+ *     Copyright (C) <2014>  <Xing Li>
+ *
+ *     RED is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     RED is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.xl.display.genomeviewer;
 
 import com.xl.datatypes.genome.Genome;
@@ -17,17 +35,17 @@ public class ChromosomeScale extends JPanel {
     private int maxLen;
 
     /**
-     * The scale used on the axis *
+     * The scale used on the axis.
      */
     private AxisScale axisScale;
 
     /**
-     * The suffix used on the scales *
+     * The suffix used on the scales.
      */
     private String scaleSuffix;
 
     /**
-     * The division on the scales *
+     * The division on the scales.
      */
     private int division;
 
@@ -106,7 +124,6 @@ public class ChromosomeScale extends JPanel {
     public Dimension getMinimumSize() {
         return new Dimension(100, 20);
     }
-
 
     /**
      * Scale x.
