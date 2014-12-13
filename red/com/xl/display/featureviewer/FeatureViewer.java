8bc5ce524508110e8fde278ab042eed97df54c77
iluhcm <sam.lxing@gmail.com>
2014-12-13 17:02:07 +0800

    The Class FeatureViewer shows the key/value annotations for a selected feature.

--- red/com/xl/display/featureviewer/FeatureViewer.java
+++ red/com/xl/display/featureviewer/FeatureViewer.java
@@ -1,21 +1,19 @@
-/**
- * Copyright Copyright 2007-13 Simon Andrews
+/*
+ * RED: RNA Editing Detector
+ *     Copyright (C) <2014>  <Xing Li>
  *
- *    This file is part of SeqMonk.
+ *     RED is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *    SeqMonk is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 3 of the License, or
- *    (at your option) any later version.
+ *     RED is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *    SeqMonk is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with SeqMonk; if not, write to the Free Software
- *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.xl.display.featureviewer;
 
@@ -30,24 +28,27 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * The Class FeatureViewer shows the key/value annotations for a selected
- * feature.
+ * The Class FeatureViewer shows the key/value annotations for a selected feature.
  */
 public class FeatureViewer extends JDialog implements MouseListener, KeyListener {
+    /**
+     * The table.
+     */
+    private JTable table = null;
+    /**
+     * The model for a feature.
+     */
+    private FeatureAttributeTable model = null;
+    /**
+     * The feature to be shown.
+     */
+    private Feature feature = null;
 
     /**
      * Instantiates a new feature viewer.
      *
-     * @param feature
-     * the feature
-     * @param application
-     * the application
+     * @param feature the feature
      */
-
-    private JTable table = null;
-    private FeatureAttributeTable model = null;
-    private Feature feature = null;
-
     public FeatureViewer(Feature feature) {
         super(REDApplication.getInstance(), "Feature: " + feature.getAliasName());
 
@@ -70,26 +71,15 @@ public class FeatureViewer extends JDialog implements MouseListener, KeyListener
 
     }
 
+    @Override
     public void mouseClicked(MouseEvent me) {
         if (me.getClickCount() == 2) {
-
-            System.err.println("Selected row is " + table.getSelectedRow()
-                    + " value is "
-                    + model.getValueAt(table.getSelectedRow(), 0));
-
-            if (model.getValueAt(table.getSelectedRow(), 0).equals(
-                    "Transcription")
-                    || model.getValueAt(table.getSelectedRow(), 0).equals(
-                    "Coding Region")
-                    || model.getValueAt(table.getSelectedRow(), 0).equals(
-                    "Exons")) {
-                DisplayPreferences.getInstance().setLocation(feature.getChr(),
-                        feature.getTxLocation().getStart(),
-                        feature.getTxLocation().getEnd());
+            Object value = model.getValueAt(table.getSelectedRow(), 0);
+            if ("Transcription".equals(value) || "Coding Region".equals(value) || "Exons".equals(value)) {
+                DisplayPreferences.getInstance().setLocation(feature.getChr(), feature.getTxLocation().getStart(), feature.getTxLocation().getEnd());
                 dispose();
             }
         }
-
     }
 
     public void mouseEntered(MouseEvent arg0) {
@@ -104,16 +94,12 @@ public class FeatureViewer extends JDialog implements MouseListener, KeyListener
     public void mouseReleased(MouseEvent arg0) {
     }
 
-
     @Override
     public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
         if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
             dispose();
         }
@@ -122,8 +108,6 @@ public class FeatureViewer extends JDialog implements MouseListener, KeyListener
 
     @Override
     public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
 }
